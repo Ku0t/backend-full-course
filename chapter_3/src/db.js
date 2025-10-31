0f91259848cf110865579b9a86b9efc4db1e815e
Ku0t <kuotthiik@gmail.com>
2025-10-31 21:52:27 +1100

    got some endpoints and emulation set up

--- chapter_3/src/db.js
+++ chapter_3/src/db.js
@@ -3,16 +3,21 @@ const db = new DatabaseSync(":memory:");
 
 // Execute SQL statements from strings
 db.exec(`
-    CREATE TABLE user (
+    CREATE TABLE users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE,
         password TEXT
     )
     `);
 
-    db.exec(`
+db.exec(`
         CREATE TABLE todos (
-            id INTEGER,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER,
+            task TEXT,
+            completed BOOLEAN DEFAULT 0,
+            FOREIGN KEY(user_id) REFERENCES users(id)
         )
-        `)
+        `);
+
+export default db;
